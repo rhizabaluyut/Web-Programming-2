0117154252a471b1095b7cbef0d272fe718b04a9
rhizabaluyut <rhizabaluyut.24@gmail.com>
2013-11-27 09:42:31 +0800

    Create front.php

---
+++ front.php
@@ -0,0 +1,38 @@
+
+<html>
+<body>
+<table border = "0">
+		<form method = "post" action = "dbconn.php">
+		
+<tr>
+		<h2>POST NEW MESSAGE</h2>
+		
+
+		
+				
+		<td>Name:
+			<td><input type = 'text' size = '30'id="name" name = 'name'>
+	
+			</td>
+<tr>
+			<td>
+		Email:<td>
+			<input type = 'text' size = '30'id="email" name = 'email'>
+		</td>
+<tr>
+	
+			<td>
+		Message:<td>
+			<textarea name="message" style="margin: 2px; width: 214px; height: 99px;"></textarea>
+		
+			</td>
+
+		</table>	
+	
+		<br>
+		<input type = 'submit' value = 'Post Message'>
+		</br>
+	
+
+</html>
+
